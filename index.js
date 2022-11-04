b58ff797c56c0ebf24b6e9e3d33c6fb95dedbdc4
RIKO910 <tarikulislamriko910@gmail.com>
2022-11-04 20:24:01 +0600

    ok

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express =require('express');
 const cors =require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 const port = process.env.PORT || 5000;
 
 app.use (cors());
 app.use(express.json())
+
+//user name:riko
+//password:zsIaHntenATorwzs
+
+
+const uri = "mongodb+srv://riko:zsIaHntenATorwzs@cluster0.ew4o60e.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+        try{
+                await client.connect();
+                const usersCollection = client.db("express").collection("users");
+                app.post('/users' ,async(req,res)=>{
+                    const newUser =req.body;
+                    console.log('adding new user',newUser);
+                    const result =await usersCollection.insertOne(newUser);
+                    res.send(result)
+                });
+        }
+        finally{
+            await client.close();
+        }
+}
+
+run().catch(console.dir);
+
 app.get('/',(req,res) =>{
     res.send("Running my Node CRUD Server");
 });
-
 
 app.listen(port,() =>{
     console.log('CRUD Server is running');
